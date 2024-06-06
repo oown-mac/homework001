b6e344f524975adb3f405dda21e90daf4384d6ae
pee4518 <pee4518@gmail.com>
2024-06-06 22:07:29 +0700

    first commit

---
+++ lib/newfile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Newfile1 extends StatelessWidget {
+  const Newfile1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+          child: Text('Hello World! oown'),
+        ),
+      );
+  }
+}
